94f2245464a926439561fa3462b2c5075afd424f
AidenKing27 <aidentheking27@hotmail.com>
2026-04-01 15:51:21 -0600

    stuff

--- DoodlesServer/Server.cs
+++ DoodlesServer/Server.cs
@@ -1,10 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 
 namespace DoodlesServer;
 
 public class Server
 {
+    private const string DELIM = "<!EOM!>";
 
+    private TcpListener listener;
+    private List<TcpClient> clients = new();
+    private bool running;
+
+    //make some eventsto handle server messages
+    public delegate void ServerMessage(Packet280 packet);
+    public event ServerMessage? ServerMessageEvent;
+
+    //another delegate to interact with our GUI
+    public delegate void LocalMessage(Packet280 packet);
+    public event LocalMessage? LocalMessageEvent;
+
+    public delegate void DisconnectMessage(string msg);
+    public event DisconnectMessage? DisconnectMessageEvent;
+
+    public Server(int port)
+    {
+        listener = new(IPAddress.Any, port);
+    }
 }
--- GameLibrary/DoodleInfo.cs
+++ GameLibrary/DoodleInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Media;
 
-namespace DoodlesClient;
+namespace GameLibrary;
 
 public class DoodleInfo
 {
